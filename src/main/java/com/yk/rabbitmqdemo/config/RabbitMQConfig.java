de87a8bd7002ef359a2f73195dc3904c11638821
gitcat7 <870375868@qq.com>
2020-03-09 20:42:07 +0800

    rabbitmq-demo

--- src/main/java/com/yk/rabbitmqdemo/config/RabbitMQConfig.java
+++ src/main/java/com/yk/rabbitmqdemo/config/RabbitMQConfig.java
@@ -1,24 +1,46 @@
 package com.yk.rabbitmqdemo.config;
 
-import com.yk.rabbitmqdemo.constant.RabbitMQConstant;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@ComponentScan({"com.yk.rabbitmqdemo.*"})
 public class RabbitMQConfig {
 
-    //创建一个消息队列
     @Bean
-    public Queue immediateQueue(){
-        //true:表示持久化
-        return new Queue(RabbitMQConstant.QUEUE_NAME,true);
+    public ConnectionFactory connectionFactory(){
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost("120.55.171.239");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        return factory;
     }
 
-//    @Bean
-//    public DirectExchange immediateExchange(){
-//        return new DirectExchange();
-//    }
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer simpleMessageListenerContainer(ConnectionFactory connectionFactory){
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory);
+        return container;
+    }
 }
--- src/test/java/com/yk/rabbitmqdemo/RabbitmqDemoApplicationTests.java
+++ src/test/java/com/yk/rabbitmqdemo/RabbitmqDemoApplicationTests.java
@@ -1,6 +1,13 @@
 package com.yk.rabbitmqdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +15,48 @@ class RabbitmqDemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private RabbitAdmin rabbitAdmin;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void test(){
+        String beanName = rabbitAdmin.getBeanName();
+        System.out.println(beanName);
+    }
+
+    @Test
+    public void testSendMessage() {
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.getHeaders().put("desc", "123");
+        String msg = "hello";
+        Message message = new Message(msg.getBytes(), messageProperties);
+
+        rabbitTemplate.convertSendAndReceive("test_exchange", "test_key", message, message1 -> {
+            System.err.println("添加额外的设置");
+            message1.getMessageProperties().getHeaders().put("desc", "111");
+            message1.getMessageProperties().getHeaders().put("test", "额外新加的属性");
+            return message1;
+        });
+    }
+
+    @Test
+    public void testSendMessage2() {
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setContentType("utf-8");
+        String msg = "hello111";
+        Message message = new Message(msg.getBytes(), messageProperties);
+
+        rabbitTemplate.convertSendAndReceive("test_exchange", "test_key", message/*, message1 -> {
+            System.err.println("添加额外的设置");
+            message1.getMessageProperties().getHeaders().put("desc", "111");
+            message1.getMessageProperties().getHeaders().put("test", "额外新加的属性");
+            return message1;
+        }*/);
     }
 
 }
